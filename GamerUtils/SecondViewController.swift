1a87e5bbb0c2ee4a349275aeca65d9be4e7ea383
jacob <github@benoits.com>
2016-01-04 19:37:40 -0800

    initial commit
    initial commit

---
+++ GamerUtils/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GamerUtils
+//
+//  Created by Jacob Benoit on 1/4/16.
+//  Copyright © 2016 Jacob Benoit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
